45d0e4e68351f17785184f3283fabc36c195c9ae
Angelo Liwanag <AngeloLiwanag75@gmail.com>
2019-10-03 13:32:08 -0700

    final

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 app.use(express.static( __dirname + '/public/dist/public' ));
-// const bcrypt = require('bcrypt');
+
 require('./backend/models/item.js');
 require('./backend/config/mongoose.js');
 
